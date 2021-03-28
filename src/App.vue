de211631e0ef35d08390255142b338256b2f7bfe
Amadej Krepek <krepekamadej@gmail.com>
2021-03-28 18:30:05 +0200

    Integrated component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <KTMethod />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import KTMethod from "./components/KTMethod";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    KTMethod
   }
 }
 </script>
