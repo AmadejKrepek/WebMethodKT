f21833442fb01a2fcc48b6765898b82d9f293615
Amadej Krepek <krepekamadej@gmail.com>
2021-03-29 21:05:54 +0200

    Added new input form

--- src/components/KTMethod.vue
+++ src/components/KTMethod.vue
@@ -29,14 +29,44 @@
           />
         </div>
       </div>
-      <div class="col-12 col-lg-2 mt-4">
+      <div class="col-12 col-lg-4 mt-4">
           <button class="btn btn-success" v-on:click="submitFile()">Submit</button>
-          <button class="btn btn-success" v-on:click="Verify()">Submit</button>
+          <button class="btn btn-success ml-3" v-on:click="Verify()">Calculate</button>
+          <button class="btn btn-success ml-3" v-on:click="Generate()">Generate graphs</button>
+      </div>
+      <div class="col-12 col-lg-3">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Izberite si parameter"
+            v-model="paramSelector"
+          />
       </div>
     </div>
     <div class="row">
       <div class="col-12 col-lg-12">
         <Graph :data="data" :result="result"/>
+      </div>
+    </div>
+        <div class="row">
+      
+      <div class="col-12 col-lg-12" v-if="gen === true">
+        <WindGraph
+          :data="altArr"
+          :labels="altTitle"
+          :chartId="'chart1'"
+          :label="'Alternatives'"
+          :borderColor="'rgb(23, 79, 45)'"
+          :type="'pie'"
+        />
+        <WindGraph
+          :data="paramArr"
+          :labels="paramTitle"
+          :chartId="'chart2'"
+          :label="'Parameters'"
+          :borderColor="'rgb(77, 39, 107)'"
+          :type="'pie'"
+        />
       </div>
     </div>
   </div>
@@ -45,21 +75,30 @@
 <script>
 import Graph from "./Table";
 import MethodKT from "../functions/MethodKT.js";
+import WindGraph from "./Chart";
 
 export default {
   name: "KTMethod",
   components: {
     Graph,
+    WindGraph
   },
   data() {
     return {
       data: [],
+      paramSelector: [],
       alternative: [],
       parameter: [],
       weight: [],
       point: [],
       check: false,
       result: [],
+      gen: false,
+      altArr: [],
+      altTitle: [],
+      paramArr: [],
+      paramTitle: [],
+      paraTitle: [],
     };
   },
   methods: {
@@ -72,6 +111,14 @@ export default {
     },
     Verify() {
       this.check = true;
+    },
+    Generate() {
+      this.altArr = MethodKT.GetAltArray();
+      this.altTitle = MethodKT.GetAltTitle();
+      this.paramArr = MethodKT.GetParamArray();
+      this.paramTitle = MethodKT.GetParamTitle();
+      MethodKT.SelectedParam(this.paramSelector);
+      this.gen = true;
     }
   },
   props: {},
