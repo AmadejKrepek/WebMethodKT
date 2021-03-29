a545b71e3b03c2fc0884f9265f7a733b1727826d
Amadej Krepek <krepekamadej@gmail.com>
2021-03-29 12:56:44 +0200

    Added props for max result

--- src/components/KTMethod.vue
+++ src/components/KTMethod.vue
@@ -31,11 +31,12 @@
       </div>
       <div class="col-12 col-lg-2 mt-4">
           <button class="btn btn-success" v-on:click="submitFile()">Submit</button>
+          <button class="btn btn-success" v-on:click="Verify()">Submit</button>
       </div>
     </div>
     <div class="row">
       <div class="col-12 col-lg-12">
-        <Graph :data="data"/>
+        <Graph :data="data" :result="result"/>
       </div>
     </div>
   </div>
@@ -57,15 +58,20 @@ export default {
       parameter: [],
       weight: [],
       point: [],
+      check: false,
+      result: [],
     };
   },
   methods: {
     submitFile() {
       let arr = MethodKT.OrganizeData(this.alternative, this.parameter, this.weight, this.point);
       const methodKT = new MethodKT(arr[0], arr[1], arr[2])
+      this.result = MethodKT.SumAndFindMaxValue(this.point, this.check, this.weight);
       this.data = methodKT;
-      console.log(methodKT)
     },
+    Verify() {
+      this.check = true;
+    }
   },
   props: {},
 };
