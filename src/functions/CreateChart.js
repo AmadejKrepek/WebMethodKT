59e1c8b9ace228f72fc338af26764c2fcb2df01d
Amadej Krepek <krepekamadej@gmail.com>
2021-03-29 21:06:34 +0200

    Added create chart values

---
+++ src/functions/CreateChart.js
@@ -0,0 +1,30 @@
+import settings from "./ChartSettings.js";
+import Chart from 'chart.js';
+
+var ChartArray = [];
+export default function createChart(chartId, values, labels, label, borderColor, type) {
+    var data = {
+        labels: labels,
+        datasets: [
+            {
+                label: label,
+                data: values,
+                fill: false,
+                borderColor: borderColor,
+                pointRadius: 0,
+            },
+        ],
+    };
+    const ctx = document.getElementById(chartId);
+    var myChart = new Chart(ctx, {
+        type: type,
+        data: data,
+        options: settings.options,
+    });
+    ChartArray.push(myChart);
+    return myChart;
+}
+
+export function updateChart() {
+    return ChartArray;
+}
