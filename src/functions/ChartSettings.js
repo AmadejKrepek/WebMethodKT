ae004482953fefd5d01a3d129e08ea27675265ae
Amadej Krepek <krepekamadej@gmail.com>
2021-03-29 21:06:19 +0200

    added chartjs settings

---
+++ src/functions/ChartSettings.js
@@ -0,0 +1,15 @@
+export const settings = {
+    data: {
+      labels: null,
+      datasets: [{
+        label: null,
+        data: null,
+        fill: false,
+        pointRadius: 0,
+      }]
+    },
+    options: {
+      responsive: true,
+    }
+  }
+  export default settings;
