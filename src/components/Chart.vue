3734070d4dd4cf3a0fbe5f3682fc0ebbef18bda5
Amadej Krepek <krepekamadej@gmail.com>
2021-03-29 21:05:45 +0200

    Added new chartjs

---
+++ src/components/Chart.vue
@@ -0,0 +1,22 @@
+<template>
+  <canvas :id="chartId"></canvas>
+</template>
+<script>
+import CreateChart from "../functions/CreateChart.js";
+export default {
+  name: "Home",
+  components: {},
+  methods: {},
+  mounted() {
+    CreateChart(this.chartId, this.data, this.labels, this.label, this.borderColor, this.type);
+  },
+  props: {
+    data: Array,
+    labels: Array,
+    chartId: String,
+    label: String,
+    borderColor: String,
+    type: String
+  },
+};
+</script>
